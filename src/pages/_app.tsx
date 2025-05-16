4946ac686456341ce5c6cd0aae2ef90d6ed8409b
lucasfralves1 <corvexbrand@gmail.com>
2025-05-16 13:51:27 -0300

    Corrige inclusão do GTM via Script no _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,26 @@
-import Script from 'next/script';
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import { Inter } from 'next/font/google';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
-const inter = Inter({
-  variable: '--font-inter-sans',
-  subsets: ['latin'],
-});
-
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Google Tag Manager Script */}
+      {/* GTM Head */}
       <Script
-        id="gtm"
+        id="gtm-head"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=GTM-NF835BF';f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NF835BF');
+            'https://www.googletagmanager.com/gtm.js?id=GTM-NF835BF'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NF835BF');
           `,
         }}
       />
 
-      <div className={`${inter.variable} antialiased`}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
-  );
+  )
 }
-
-export default MyApp;
-
